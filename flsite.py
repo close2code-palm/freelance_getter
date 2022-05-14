fe0000e4e13d0aaf08f93a3ee99bf511efef4196
Georgyyx <julyfortune101@gmail.com>
2022-05-14 22:55:10 +0300

    inherited habr class with and its entity

--- flsite.py
+++ flsite.py
@@ -6,11 +6,12 @@ import uuid
 
 import bs4
 import requests
+from aiohttp import ClientSession
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
-from part_time import WorkHeaders
+from part_time import WorkHeaders, HabrWorkHeader
 
 
 # Need to be splited into parser and site_handler
@@ -20,7 +21,8 @@ class Site:
     def get_scraped(self):
         return self.name, self.job_headers
 
-    def __init__(self, url, name, act_counter, suffs, *containers, js=False):
+    def __init__(self, url, name, act_counter,
+                 suffs, *containers, js_rends=False):
         self.url = url
         self.name = name
         self.job_suffixes = suffs
@@ -28,7 +30,7 @@ class Site:
         self.html_cont = containers
         # TODO possibly extensible for page rendering mechanics check
         # if 'document.write' in ...
-        self.dyn = js
+        self.dyn = js_rends
         # TODO store to not repeat
         # UNIQUE in db or watcher
         self.job_headers = []
@@ -62,13 +64,15 @@ class Site:
 
     def scrape(self, theme, needed=1):
         """getting offers from all but 1st page to list """
-        _scr_counter = min(needed, self.act_counter)
+        # _scr_counter = min(needed, self.act_counter)
         last_sup = ""
-        for c in range(1, _scr_counter + 1):
+        for c in range(1, needed + 1):
             # here MUST BE IMPLEMENTED CHECK FOR REPETITION
-            scr_url = self._url_constr(theme, initial=False) + str(c)
+            scr_url = self._url_constr(theme, False) + str(c)
             fl_sup = self._get_soup(scr_url)
             # TODO returns function to thread? after check
+            # possible sol: append(thread(args));excute;join
+
             if last_sup != fl_sup:
                 ziped_conts = self._jobs_data(fl_sup)
                 self.ent_gen(ziped_conts)
@@ -78,9 +82,9 @@ class Site:
     # TODO normalize utils
     def ent_gen(self, parameters):
         """constructing job item for processing"""
-        for jt, jp, jd in parameters:
-            head = WorkHeaders(str(uuid.uuid4()), jt.text.strip(), jp.text.strip(),
-                               jd.text.strip(), self.name)
+        for j_t, j_p, j_d in parameters:
+            head = WorkHeaders(str(uuid.uuid4()), j_t.text.strip(), j_p.text.strip(),
+                               j_d.text.strip(), self.name)
             self.job_headers.append(head)
 
     # TODO make decorator for concurrency
@@ -97,13 +101,36 @@ class Site:
 
         if self.dyn:
             page = selenm_get(url)
-            bs = bs4.BeautifulSoup(page, 'html.parser')
+            btfl_sp = bs4.BeautifulSoup(page, 'html.parser')
         else:
             page = requests.get(url)
-            bs = bs4.BeautifulSoup(page.text, 'html.parser')
-        return bs
+            btfl_sp = bs4.BeautifulSoup(page.text, 'html.parser')
+        return btfl_sp
 
-# TODO should be moved to storage
+    # TODO should be moved to storage
     def show(self):
         for header in self.job_headers:
             print(self.name, header)
+
+
+class HabrFlSite(Site):
+
+    def __init__(self, url, name, act_counter,
+                 suffs, *containers):
+        super().__init__(url, name, act_counter,
+                         suffs, *containers, js_rends=False)
+        self.tags_lists = [('ul', {'class': 'tags_short'})]
+        self.tags_concrete = 'a', {'class': 'tags__item_link'}
+        self.tags_lists.extend(list(self.html_cont))
+        self.html_cont = self.tags_lists
+
+
+    def ent_gen(self, parameters):
+        for tags_soup, j_t, j_p, j_d in parameters:
+            tags = tags_soup.findall(*self.tags_concrete)
+            habr_proposal = HabrWorkHeader(str(uuid.uuid4()),
+                                           j_t.text.strip(), j_p.text.strip(),
+                                           j_d.text.strip(), self.name,
+                                           [tag.text for tag in tags])
+            self.job_headers.append(habr_proposal)
+
